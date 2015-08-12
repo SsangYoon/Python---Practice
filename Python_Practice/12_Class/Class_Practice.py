79e5f247325950e1195cfe6ef7d7376daaac9b97
SsangYoon <Sangyun@isang-yun-ui-MacBook-Pro.local>
2015-08-12 15:57:45 +0900

    Updated 10,11_Practice

---
+++ Python_Practice/12_Class/Class_Practice.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2015. 8. 12.
+@attention: About Class 
+@author: Sangyun
+'''
+
+class Person:
+    def __init__(self):     # 생성자 
+                        # 함수 아래에 아무라인이 없으면 
+    def Set_State(self, Name, Height, Weight) :
+        self.Name = Name
+        self.Height = Height
+        self.Weight = Weight
